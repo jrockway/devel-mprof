66045e914fc339380111bb22734fd43e8ec5066f
Jonathan Rockway <jon@jrock.us>
2007-08-08 02:18:21 -0500

    initial version

---
+++ lib/Devel/MProf.pm
@@ -0,0 +1,13 @@
+# Copyright (c) 2007 Jonathan Rockway <jrockway@cpan.org>
+
+package Devel::MProf;
+use strict;
+use warnings;
+
+our $VERSION = '0.00_00';
+
+sub DB::sub {
+    die "a sub was called.  very good";
+}
+
+1;
